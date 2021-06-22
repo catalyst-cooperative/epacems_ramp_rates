726185ec7e10a820928cc5f0dc7682bf82c8d825
t-desktop <tpbush@gmail.com>
2021-06-22 16:56:59 -0700

    document loading function

--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -22,18 +22,19 @@ def load_epacems(
     """load EPA CEMS data from PUDL with optional subsetting
 
     Args:
-        states (Optional[Sequence[str]], optional): subset by state abbreviation. Pass None to not filter. Defaults to ("CO",).
-        years (Optional[Sequence[int]], optional): subset by year. Pass None to not filter. Defaults to (2019,).
-        columns (Optional[Sequence[str]], optional): subset by column. Pass None to not filter. Defaults to ( "plant_id_eia", "unitid", "operating_datetime_utc", "operating_time_hours", "gross_load_mw", "state", ).
+        states (Optional[Sequence[str]], optional): subset by state abbreviation. Pass None to get all states. Defaults to ("CO",).
+        years (Optional[Sequence[int]], optional): subset by year. Pass None to get all years. Defaults to (2019,).
+        columns (Optional[Sequence[str]], optional): subset by column. Pass None to get all columns. Defaults to ( "plant_id_eia", "unitid", "operating_datetime_utc", "operating_time_hours", "gross_load_mw", "state", ).
+        engine (Optional[str], optional): choose 'pandas' or 'dask'. Defaults to 'pandas'
 
     Returns:
         pd.DataFrame: epacems data
     """
     if states is None:
-        states = pudl.constants.us_states.keys()
+        states = pudl.constants.us_states.keys()  # all states
     if years is None:
-        years = pudl.constants.data_years["epacems"]
-    # columns=None is handled by pd.read_parquet
+        years = pudl.constants.data_years["epacems"]  # all years
+    # columns=None is handled by pd.read_parquet, gives all columns
     if engine != "pandas":
         raise NotImplementedError("dask engine not yet implemented. Only pandas")
 
