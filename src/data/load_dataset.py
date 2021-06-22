29cff16f99fc3d85c775254bb320288bde37126f
t-desktop <tpbush@gmail.com>
2021-06-22 16:29:12 -0700

    make dataset loading function using pandas

---
+++ src/data/load_dataset.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from typing import Sequence, Optional
+
+import pandas as pd
+import pudl
+
+
+def load_epacems(
+    states: Optional[Sequence[str]] = ("CO",),
+    years: Optional[Sequence[int]] = (2019,),
+    columns: Optional[Sequence[str]] = (
+        "plant_id_eia",
+        "unitid",
+        "operating_datetime_utc",
+        "operating_time_hours",
+        "gross_load_mw",
+        "state",
+    ),
+    engine: Optional[str] = "pandas",
+) -> pd.DataFrame:
+    """load EPA CEMS data from PUDL with optional subsetting
+
+    Args:
+        states (Optional[Sequence[str]], optional): subset by state abbreviation. Pass None to not filter. Defaults to ("CO",).
+        years (Optional[Sequence[int]], optional): subset by year. Pass None to not filter. Defaults to (2019,).
+        columns (Optional[Sequence[str]], optional): subset by column. Pass None to not filter. Defaults to ( "plant_id_eia", "unitid", "operating_datetime_utc", "operating_time_hours", "gross_load_mw", "state", ).
+
+    Returns:
+        pd.DataFrame: epacems data
+    """
+    if states is None:
+        states = pudl.constants.us_states.keys()
+    if years is None:
+        years = pudl.constants.data_years["epacems"]
+    # columns=None is handled by pd.read_parquet
+    if engine != "pandas":
+        raise NotImplementedError("dask engine not yet implemented. Only pandas")
+
+    pudl_settings = pudl.workspace.setup.get_defaults()
+    cems_path = Path(pudl_settings["parquet_dir"]) / "epacems"
+    cems = pd.read_parquet(
+        cems_path,
+        use_nullable_dtypes=True,
+        columns=columns,
+        filters=pudl.output.epacems.year_state_filter(
+            states=states,
+            years=years,
+        ),
+    )
+    return cems
