c33e52519d84c266c825597b0666e5f8a1bfa01a
t-desktop <tpbush@gmail.com>
2021-06-21 17:52:11 -0700

    initial template workspace

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Characterization of power plant ramp rates using hourly EPA CEMS data',
+    author='Catalyst Cooperative',
+    license='MIT',
+)
